96824a3f2cfe1bdadba9423f28ac44a93db7338f
Adrian Lozano <adrian.loma@gmail.com>
2014-05-05 00:15:53 -0500

    script for DB tables, fixed

--- Scripts/createTables.sql
+++ Scripts/createTables.sql
@@ -1,4 +1,24 @@
-CREATE TABLE suscripciones (
+use AMSS;
+
+CREATE TABLE IF NOT EXISTS usuarios (
+			IdUsuario INT NOT NULL AUTO_INCREMENT,
+			Tipo VARCHAR(255),
+			Nombre VARCHAR(255),
+			FechaDeNacimiento DATETIME,
+			Email VARCHAR(255),
+			Password VARCHAR(255),
+			PRIMARY KEY (IdUsuario)
+			);
+
+CREATE TABLE IF NOT EXISTS votos (
+			IdVoto INT AUTO_INCREMENT,
+			IdUsuario INT,
+			IdArticulo INT,
+			Favor BOOLEAN,
+			PRIMARY KEY (IdVoto)
+			);
+
+CREATE TABLE IF NOT EXISTS suscripciones (
 			IdSuscripcion INT NOT NULL AUTO_INCREMENT,
 			IdSuscriptor INT NOT NULL,
 			FechaDeInicio DATETIME,
@@ -7,42 +27,26 @@ CREATE TABLE suscripciones (
 			FOREIGN KEY (IdSuscriptor) REFERENCES usuarios (IdUsuario)
 			);
 
-CREATE TABLE usuarios (
-			IdUsuario NOT NULL AUTO_INCREMENT,
-			Tipo VARCHAR(255),
-			Nombre VARCHAR(255),
-			FechaDeNacimiento DATETIME,
-			Email VARCHAR(255),
-			Password VARCHAR(255)
-			);
-
-CREATE TABLE articulos (
+CREATE TABLE IF NOT EXISTS articulos (
 			IdArticulo INT NOT NULL AUTO_INCREMENT,
 			Titulo VARCHAR (255),
 			IdAutor INT NOT NULL,
 			FechaPublicacion DATETIME,
 			Publicado BOOLEAN,
-			Informacion MEDIUMTEXT
+			Informacion MEDIUMTEXT,
 			PRIMARY KEY (IdArticulo),
 			FOREIGN KEY (IdAutor) REFERENCES usuarios (IdUsuario)
 			);
 
-CREATE TABLE votos (
-			IdVoto INT AUTO_INCREMENT,
-			IdUsuario INT,
-			IdArticulo INT,
-			Favor BOOLEAN,
-			PRIMARY KEY (IdVoto)
-			);
 
-CREATE TABLE publicidad (
+CREATE TABLE IF NOT EXISTS publicidad (
 			IdPublicidad INT NOT NULL AUTO_INCREMENT,
 			IdUsuario INT,
 			IdArticulo INT,
 			Informacion TEXT,
 			FechaPublicacion DATETIME,
-			Publicado BOOLEAN
+			Publicado BOOLEAN,
 			PRIMARY KEY (IdPublicidad),
 			FOREIGN KEY (IdUsuario) REFERENCES usuarios (IdUsuario),
-			FOREIGN KEY (IdArticulo) REFERENCES usuarios (articulos)
+			FOREIGN KEY (IdArticulo) REFERENCES articulos (IdArticulo)
 			);
