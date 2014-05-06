45ba98241c0eb1166a1cfc9ab1c3756a4d312337
jauregui <gmjauregui@gmail.com>
2014-05-06 02:18:21 -0500

    Update Articulo.java

--- Entidades/Articulo.java
+++ Entidades/Articulo.java
@@ -14,14 +14,28 @@ public class Articulo{
 
    }
 	   
-	public String getArticulo(int idArticulo) {	
-    try {
-      stmt.executeQuery("SELECT informacion FROM articulo WHERE idArticulo = " + idArticulo);
-      ResultSet rs = stmt.getResultSet();
-      if (rs.next()) {
-      	return rs.getString("informacion");
-      }
-    } catch (SQLException e) { System.out.println ("Error reading database validarPorNombre"); }
+	public  String getArticulo(int idArticulo) {	
+    		try {
+      			stmt.executeQuery("SELECT informacion FROM articulo WHERE idArticulo = " + idArticulo);
+      			ResultSet rs = stmt.getResultSet();
+      			if (rs.next()) {
+      				return rs.getString("informacion");
+      					}
+    			} catch (SQLException e) { System.out.println ("Error reading database validarPorNombre"); }
     return "";
+	}
+	
+	public boolean editArticulo(int idArticulo, String nInfo, String nTitulo)
+	{
+		try{
+			stmt.executeQuery("SELECT informacion FROM articulo WHERE idArticulo = " + idArticulo);
+			ResultSet rs= stmt.getResultSet();
+			if(rs.next()){
+				if(nInfo!="")
+				stmt.executeUpdate("UPDATE articulo SET informacion = '" + nInfo + "'");
+				if(nTitulo!="")
+				stmt.excecuteUpdate("UPDATE articulo SET titulo = '" + nTitulo + "'");
+			}
+		}
 	}
 }
